392c896629a56d33ad46bd58aa948bd54a79fee5
Artur Karamov <karamov.1311@yandex.ru>
2023-12-01 21:43:26 +0300

    deploy backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './.env.deploy' });
 
 // eslint-disable-next-line object-curly-newline
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF } = process.env;
@@ -20,7 +20,8 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build && pm2 startOrRestart ecosystem.config.js',
+      'post-deploy':
+        'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
