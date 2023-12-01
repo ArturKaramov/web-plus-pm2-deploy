db8619486ec7ecf3937a14cb1a5c2b2fc67627a6
Artur Karamov <karamov.1311@yandex.ru>
2023-12-01 22:43:50 +0300

    deploy frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+// eslint-disable-next-line object-curly-newline
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF } = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+    },
+  },
+};
